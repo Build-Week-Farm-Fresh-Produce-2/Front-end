6dfbfa0ff4706c3cb528a55e8006a30225211625
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2019-12-27 20:16:12 -0500

    shopping cart and shopping item

--- farm-fresh-produce/src/Components/ShoppingCart.js
+++ farm-fresh-produce/src/Components/ShoppingCart.js
@@ -1,11 +1,28 @@
-import React, {useContext} from 'react';
-import {Route} from 'react-router-dom';
-import ShoppingContext from '../Contexts/ShoppingContext';
+import React, { useContext } from "react";
+import ShoppingContext from "../Contexts/ShoppingContext";
+import Item from "./ShoppingCartItem";
 
-export default function ShoppingCart (){
-    return (
-        <Cart>
-            
-        </Cart>
-    )
+export default function ShoppingCart() {
+  const { cart, removeItem } = useContext(ShoppingContext);
+
+  const orderTotal = () => {
+    return cart
+      .reduce((acc, value) => {
+        return acc + value.price;
+      }, 0)
+      .toFixed(2);
+  };
+
+  return (
+    <Cart>
+      {cart.map(item => (
+        <Item key={item.id} removeItem={removeItem} {...item} />
+      ))}
+
+      <div>
+        <p>Total: ${orderTotal()}</p>
+        <button>Checkout</button>
+      </div>
+    </Cart>
+  );
 }
