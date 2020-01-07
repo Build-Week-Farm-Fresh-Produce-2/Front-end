0a66cabc06d9fb1fd11ed5e3073b84993d752031
garybot <argot.pryer@gmail.com>
2020-01-06 20:54:44 -0800

    Working out user models and state struture.

--- farm-fresh-produce/src/Reducers/reducer.js
+++ farm-fresh-produce/src/Reducers/reducer.js
@@ -1,8 +1,23 @@
-export const initialState = {produce: []};
+export const initialState = {user: {name: "", grower: false, cart: []}, produce: []};
 
 export const reducer = (state=initialState, action) => {
   switch(action.type) {
+    "LOGIN_SUCCESS":
+      return {...state, user: action.payload};
+    "LOGIN_FAILURE":
+      return state;
     default:
       return state;
   }
 }
+
+// USER MODELS 
+/*
+user = {
+  username:,
+  id:,
+  grower: true/false,
+  inventory: null/[],
+  cart: []
+}
+*/
