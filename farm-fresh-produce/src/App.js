cc3c4342992c46065d2967a7a8c623d84dca73ec
garybot <argot.pryer@gmail.com>
2020-01-09 12:29:44 -0800

    Reworked Nav and App routes, still rough.

--- farm-fresh-produce/src/App.js
+++ farm-fresh-produce/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import {connect} from 'react-redux';
-
 import './App.css';
 
+// COMPONENTS
+import Nav from './Components/Nav';
 import Dashboard from './Components/Dashboard';
 import Login from './Components/Login';
 import SignUp from './Components/SignUp';
+import ProduceList from './Components/ProduceList';
+import ShoppingCart from './Components/ShoppingCart';
 
 function App(props) {
   return (
     <div className="App">
-      <h1>Farm Fresh Produce</h1>
-      <Link to="dashboard">Dashboard</Link>
-      <Route path="/"><SignUp /></Route>
-      <Route path="/"><Login /></Route>
-      <Route path="/"><Dashboard user={props.user} /></Route>
+      <Route path="/" component={Nav} />
+      <Route path="/dashboard"><Dashboard user={props.user} /></Route>
+      <Route path="/register" component={SignUp} />
+      <Route path="/login" component={Login} />
+      <Route exact path="/shop" component={ProduceList} />
+      <Route exact path="/cart" component={ShoppingCart} />
     </div>
   );
 }
--- farm-fresh-produce/src/Components/Nav.js
+++ farm-fresh-produce/src/Components/Nav.js
@@ -1,25 +1,35 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
     <div>
-      <Nav>
-        <Logo className="placeholder for logo from Web 1"></Logo>
-        <NavLinks>
-          {/* connect to web 1 */}
-          <a href="/">Home</a>
-          <a href="/about">About Us</a>
-          <Users>
-            <Route exact path={`/register`}>
-              Sign Up
-            </Route>
-            <Route exact path={`/signin`}>
-              Log In
-            </Route>
-          </Users>
-        </NavLinks>
-      </Nav>
+      {
+      //<Logo className="placeholder for logo from Web 1"></Logo>
+      }
+      <h1>Market Direct</h1>
+      <nav>
+        {/* connect to web 1 */}
+        <Link to="/">Home</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/dashboard/orders">View Orders</Link>
+        <Link to="/dashboard/inventory">View Inventory</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/cart">View Cart</Link>
+        <Link to="/checkout">Checkout</Link>
+        <Link to="/login">Login</Link>
+      </nav>
     </div>
   );
 }
+
+
+          // <Users>
+          //   <Route exact path={`/register`}>
+          //     Sign Up
+          //   </Route>
+          //   <Route exact path={`/signin`}>
+          //     Log In
+          //   </Route>
+          // </Users>
