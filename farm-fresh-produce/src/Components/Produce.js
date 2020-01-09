2dfe4a9864d719c9691648cacc31fd555c8c5f65
garybot <argot.pryer@gmail.com>
2020-01-08 18:06:27 -0800

    added alt property to img tag.

--- farm-fresh-produce/src/Components/Produce.js
+++ farm-fresh-produce/src/Components/Produce.js
@@ -4,7 +4,7 @@ const Produce = props => {
   const item = props.item;
   return (
     <div className="produce">
-      <img src={"placeholder for now"}></img>
+      <img alt={item.name} src={"placeholder for now"}></img>
       <h3 className="name">{item.name}</h3>
       <p className="price">{item.price}</p>
       <p className="unit">{item.unit}</p>
