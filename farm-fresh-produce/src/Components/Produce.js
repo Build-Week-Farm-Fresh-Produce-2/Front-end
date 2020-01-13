4458b0b01ee8e003a3440b03a527c6546a164c8f
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2020-01-12 20:51:23 -0500

    added some style to produce card, waving the white flag

--- farm-fresh-produce/src/Components/Produce.js
+++ farm-fresh-produce/src/Components/Produce.js
@@ -1,17 +1,37 @@
 import React from "react";
+import {Card} from '@material-ui/core';
+import {makeStyles} from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: '#F9931D',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px #AC54AE',
+    color: '#5A5A5A',
+    height: 160,
+    width: 300,
+    padding: '0 30px',
+    marginTop: '5px',
+    flexWrap: 'wrap',
+  },
+});
 
 const Produce = props => {
   const item = props.item;
+  const classes=useStyles();
   return (
-    <div className="produce">
-      <img alt={item.name} src={"placeholder for now"}></img>
-      <h3 className="name">{item.name}</h3>
-      <p className="price">{item.price}</p>
-      <p className="unit">{item.unit}</p>
-      <p className="deets">{item.description}</p>
-      { /* <button onClick={() => props.addToCart(item)}>
-        Add to cart
-      </button>     */  }
+    <div className="produce" style={{padding: 15}}>
+      <Card flexDirection="row" variant="outlined" className={classes.root}>
+        {/* <img alt={item.name} src={"placeholder for now"}></img> */}
+        <h3 className="name">{item.name}</h3>
+        <p className="price">{item.price}</p>
+        <p className="unit">{item.unit}</p>
+        <p className="deets">{item.description}</p>
+        { /* <button onClick={() => props.addToCart(item)}>
+          Add to cart
+        </button>     */  }
+      </Card>
     </div>
   );
 };
